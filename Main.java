123f3b797121e418643301ffd123b4892b599af7
Jxel117 <stalintapia@unl.edu.ec>
2024-05-04 21:32:25 -0500

    Agregacion de materias 2do ciclo

--- Main.java
+++ Main.java
@@ -5,16 +5,17 @@ import mallaCurricular.*;
 public class Main {
     public static void main(String[] args) {
         Facultad facultad = new Facultad("Facultad de Energía", "FEIRNNR"); // Insta
-        Carrera computacion = new Carrera("123", "Computación",
-                "Ingeniero en computacion", facultad, Modalidad.PRESENCIAL);
+        Carrera computacion = new Carrera("123", "Computación", "Ingeniero en computacion", facultad, Modalidad.PRESENCIAL);
         facultad.getCarreraList().add(computacion);
         facultad.getCarreraList().add(new Carrera("124", "Electronica",
                 "Ingeniero en electronica", facultad, Modalidad.PRESENCIAL));
 
        computacion.crearCiclos(9);
-       computacion.crearAsignatura(2, "POO");
-       computacion.crearAsignatura(2, "Análisis");
-       computacion.crearAsignatura(2, "Matemática");
+       computacion.crearAsignatura(2, "Programacio Orientada a Objetos");
+       computacion.crearAsignatura(2, "Análisis matemático");
+       computacion.crearAsignatura(2, "Diseño de circuitos");
+       computacion.crearAsignatura(2, "Emprendimiento e Innovacion Tecnologica");
+       computacion.crearAsignatura(2, "Teoria de la distribución y la probabilidad");
        System.out.println(facultad);
     }
 }
